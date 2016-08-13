ea91d8c04c723addb2791f9360a0f328c5cbf3d4
Tsukasa Arima <pyar6329@gmail.com>
2016-08-13 19:02:52 +0900

    dockerize using only having DB_HOST

--- prod/docker-entrypoint.sh
+++ prod/docker-entrypoint.sh
@@ -4,7 +4,9 @@ set -x
 if [ "$1" = 'unicorn_rails' ]; then
     cd /usr/src/app
     RAILS_VERSION=$(bundle exec rails -v | awk '{print $2}' | tr '.' ' ' | awk '{print $1}')
-    dockerize -wait tcp://$DB_HOST:$DB_PORT -timeout 300s
+    if [[ -n $DB_HOST ]]; then
+      dockerize -wait tcp://$DB_HOST:$DB_PORT -timeout 300s
+    fi
     if [ $RAILS_VERSION -ge 5 ]; then
       rails assets:precompile
       rails db:migrate
